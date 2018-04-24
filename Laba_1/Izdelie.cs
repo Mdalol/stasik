6ece49b4091a8e5eb5b06b75671da0ea46a46353
Mdalol <stas-limon99@mail.ru>
2018-04-24 09:07:20 +0300

    DVP

--- Laba_1/Izdelie.cs
+++ Laba_1/Izdelie.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Laba_1
 {
+    /// <summary>
+    ///     Описываем класс Изделие
+    /// </summary>
     class Izdelie
     {
+        public string Name { get; set; }
+        public string Shift { get; set; }
+        public int Kol { get; set; }
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Izdelie()
+        {
+        }
+        /// <summary>
+        /// Ввод объектов класса с клавиатуры
+        /// </summary>
+        public void Input()
+        {
+            Name = Console.ReadLine();
+            Shift = Console.ReadLine();
+            Kol = int.Parse(Console.ReadLine());
+        }
     }
 }
--- Laba_1/Program.cs
+++ Laba_1/Program.cs
@@ -10,6 +10,7 @@ namespace Laba_1
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
